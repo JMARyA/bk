bf53a14d610ed5752030ac2115e11694f2f62393
JMARyA <jmarya@hydrar.de>
2025-08-02 23:28:51 +0200

    update

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use yansi::{Color, Paint};
+
+pub mod backup;
+pub mod config;
+pub mod restic;
+
+
+pub fn run_command(cmd: &[&str], env: Option<Vec<(String, String)>>) -> (String, String) {
+    println!("--> {} ", cmd.join(" ").paint(Color::Blue));
+
+    let mut cmd_setup = std::process::Command::new(cmd[0]);
+    let mut cmd_setup = cmd_setup.args(cmd.iter().skip(1).collect::<Vec<_>>());
+
+    cmd_setup = cmd_setup
+        .stdout(std::process::Stdio::inherit())
+        .stdin(std::process::Stdio::inherit());
+
+    if let Some(pw) = env {
+        for e in pw {
+            cmd_setup = cmd_setup.env(e.0, e.1);
+        }
+    }
+
+    let child = cmd_setup.spawn().unwrap();
+
+    let status = child.wait_with_output().unwrap();
+    if !status.status.success() {
+        println!(
+            "{} Command {} returned with non zero exit code.",
+            "Error:".paint(Color::Red),
+            cmd.join(" ")
+        );
+    }
+
+    let output = String::from_utf8(status.stdout).unwrap();
+    let stderr = String::from_utf8(status.stderr).unwrap();
+
+    if !stderr.trim().is_empty() {
+        eprintln!("{stderr}");
+    }
+
+    (output, stderr)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use backup::run_backup;
-use yansi::{Color, Paint};
-
-mod backup;
-mod config;
-mod restic;
+use bk::backup::run_backup;
 
 // TODO : add basic ctrl+c support for ending bk tasks instead of everything and ensure cleanups
 
@@ -16,41 +11,4 @@ fn main() {
     } else {
         println!("Usage: bk <config>");
     }
-}
-
-pub fn run_command(cmd: &[&str], env: Option<Vec<(String, String)>>) -> (String, String) {
-    println!("--> {} ", cmd.join(" ").paint(Color::Blue));
-
-    let mut cmd_setup = std::process::Command::new(cmd[0]);
-    let mut cmd_setup = cmd_setup.args(cmd.iter().skip(1).collect::<Vec<_>>());
-
-    cmd_setup = cmd_setup
-        .stdout(std::process::Stdio::inherit())
-        .stdin(std::process::Stdio::inherit());
-
-    if let Some(pw) = env {
-        for e in pw {
-            cmd_setup = cmd_setup.env(e.0, e.1);
-        }
-    }
-
-    let child = cmd_setup.spawn().unwrap();
-
-    let status = child.wait_with_output().unwrap();
-    if !status.status.success() {
-        println!(
-            "{} Command {} returned with non zero exit code.",
-            "Error:".paint(Color::Red),
-            cmd.join(" ")
-        );
-    }
-
-    let output = String::from_utf8(status.stdout).unwrap();
-    let stderr = String::from_utf8(status.stderr).unwrap();
-
-    if !stderr.trim().is_empty() {
-        eprintln!("{stderr}");
-    }
-
-    (output, stderr)
 }
