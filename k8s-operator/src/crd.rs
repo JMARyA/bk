783289ebe8c249863ba4a3f0c0ccdaf8010298d6
JMARyA <jmarya@hydrar.de>
2025-08-04 17:36:05 +0200

    update

--- k8s-operator/src/crd.rs
+++ k8s-operator/src/crd.rs
@@ -2,6 +2,7 @@ use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/// Restic Repository
 #[derive(CustomResource, Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
 #[kube(
     group = "bk.jmarya.me",
@@ -12,9 +13,13 @@ use serde::{Deserialize, Serialize};
     namespaced
 )]
 pub struct ResticRepositorySpec {
+    /// The endpoint of the repository. Can be either a path, s3 or sftp.
     pub endpoint: String,
+    /// S3 credentials
     pub s3: Option<S3Config>,
+    /// SSH credentials
     pub ssh: Option<SSHConfig>,
+    /// Repository password
     pub passphrase: SecretKeyRef,
 }
 
@@ -36,6 +41,7 @@ pub struct SecretKeyRef {
     pub secretKey: String,
 }
 
+/// Resource for backing up the filesystem of a node
 #[derive(CustomResource, Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
 #[kube(
     group = "bk.jmarya.me",
@@ -46,10 +52,16 @@ pub struct SecretKeyRef {
     namespaced
 )]
 pub struct NodeBackupSpec {
+    /// Target repository
     pub repository: String,
+    /// Source paths to backup
     pub paths: Vec<String>,
+    /// Cron schedule for the backup
     pub schedule: String,
+    /// Which node the backup runs on
     pub node: String,
+    /// Quiet mode
     pub quiet: Option<bool>,
+    /// Create a cephFS snapshot before backup
     pub cephfs_snap: Option<bool>,
 }
